1081ce8cfeb67c6acea53361c49d30e6da7fb9ac
vlad polevoi <vlad.polewoi1@gmail.com>
2023-07-30 22:01:31 +0300

    feat: :sparkles: folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button"
+
+const DashboardPage = () => {
+	return <Button variant="destructive">Click me</Button>
+}
+
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+interface pageProps {}
+
+const LandingPage: React.FC<pageProps> = ({}) => {
+	return <div>LandingPage</div>
+}
+
+export default LandingPage
